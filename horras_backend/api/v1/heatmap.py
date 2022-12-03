f3d5deda1b0ef20a0d70f580a3a99b8eb56b2110
yanglinshu <nicolasvandarkholme@outlook.com>
2022-12-03 15:26:37 +0800

    Add lazy load for init

--- horras_backend/api/v1/heatmap.py
+++ horras_backend/api/v1/heatmap.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict
 from fastapi import APIRouter
 # import horras_backend.model.heatmap.queries as queries
 import horras_backend.model.heatmap.cheat as cheat
@@ -6,12 +6,16 @@ import horras_backend.model.heatmap.cheat as cheat
 router = APIRouter()
 
 @router.get("/")
-async def headmap() -> List[List[int]]:
+async def headmap() -> Dict[str, List[List[int]]]:
     # clock_range = queries.CLOCK_UPPER_BOUND - queries.CLOCK_LOWER_BOUND
     # date_range = queries.DATE_UPPER_BOUND - queries.DATE_LOWER_BOUND
     # headmap_values = [[0 for i in range(clock_range // 60)] for j in range(date_range)]
     # for date in range(1, date_range):
     #     for clock in range(0, clock_range, 60):
     #         headmap_values[date][clock // 60] = await queries.find_records_count(date, date + 1, clock, clock + 60)
-    data = cheat.get_headmap()
-    return data
+    heatmap = cheat.get_headmap()
+    init_geometry = cheat.get_init_geometry()
+    return {
+        "heatmap": heatmap,
+        "init_geometry": init_geometry
+    }
--- horras_backend/model/heatmap/cheat.py
+++ horras_backend/model/heatmap/cheat.py
@@ -1,10 +1,17 @@
 import json
 import os
-from typing import List
+from typing import List, Dict, Union
 from horras_backend.config import src_folder
 
 def get_headmap() -> List[List[int]]:
     path = os.path.join(src_folder, "model", "heatmap", "heatmap.json")
+    with open(path) as json_file:
+        data = json.load(json_file)
+
+    return data
+
+def get_init_geometry() -> List[Dict[str, Union[int, float]]]:
+    path = os.path.join(src_folder, "model", "heatmap", "init_geometry.json")
     with open(path) as json_file:
         data = json.load(json_file)
 
